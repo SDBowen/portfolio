d1f35c1e5ddd284aec1c70155bc1ddc6b5525405
Steven Bowen <sdbowen.git@gmail.com>
2019-01-20 15:10:55 -0700

    Format date on repo section

--- js/githubData.js
+++ js/githubData.js
@@ -33,12 +33,47 @@ let updatePageElements = data => {
   let languageElements = document.querySelectorAll(".repo-item__language-text");
 
   for (let i = 0; i < 3; i++) {
-    dateElements[i].innerHTML = data[i].updatedDate;
+    dateElements[i].innerHTML = formatDate(data[i].updatedDate);
     nameElements[i].innerHTML = data[i].name;
     descElements[i].innerHTML = data[i].description;
     languageElements[i].innerHTML = data[i].language;
     languageElements[i].className += ` repo-item__language-text--${data[
       i
     ].language.toLowerCase()}`;
+  }
+};
+
+let formatDate = repoItemDate => {
+  let year = repoItemDate.slice(0, 4);
+  let month = repoItemDate.slice(5, 7) - 1;
+  let day = repoItemDate.slice(8, 10);
+  let hour = repoItemDate.slice(11, 13);
+  let minute = repoItemDate.slice(14, 16);
+  let seconds = repoItemDate.slice(17, 19);
+
+  let repoDate = new Date(Date.UTC(year, month, day, hour, minute, seconds));
+  let currentDate = new Date(Date.now());
+
+  let oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  let oneHour = 60 * 60 * 1000;
+
+  let difference = Math.round(
+    Math.abs((repoDate.getTime() - currentDate.getTime()) / oneDay)
+  );
+
+  if (difference > 1) {
+    return `Updated ${difference} days ago`;
+  } else if (difference === 1) {
+    return "Updated 1 day ago";
+  }
+
+  difference = Math.round(
+    Math.abs((repoDate.getTime() - currentDate.getTime()) / oneHour)
+  );
+
+  if (difference > 1) {
+    return `Updated ${difference} hours ago`;
+  } else {
+    return "Updated 1 hour ago";
   }
 };
